eda70115abe708819eb4b19abf0d94be06d493cc
gabriell_munhoz@icloud.com <gabriell_munhoz@icloud.com>
2018-05-28 23:50:15 -0300

    Algumas mudancas

--- src/Aplicacao.java
+++ src/Aplicacao.java
@@ -2,7 +2,16 @@
 public class Aplicacao {
 
 	public static void main(String[] args) {
-		
+		Vetor t; 
+		t= new Vetor();
+
+		t.inserefinal(1);
+
+		if(t.isEmpty()){
+			System.out.println("Vetor esta vazio! ");
+		}else{
+			System.out.println("Vetor nao esta vazio!");
+		}
 	}
 
 }
